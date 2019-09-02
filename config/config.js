6ba48ab587784dbf103b53f5c1fc8ecd8dc2eceb
Marius Petcu <dapetcu21@gmail.com>
2019-09-02 19:40:44 +0300

    Change development db name

--- config/config.js
+++ config/config.js
@@ -3,13 +3,11 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "database": "da_petcu21",
+    "database": "kazoo_snapshot_development",
     "host": "127.0.0.1",
     "dialect": "postgres",
   },
   "test": {
-    "username": "root",
-    "password": null,
     "database": "kazoo_snapshot_test",
     "host": "127.0.0.1",
     "dialect": "postgres",
